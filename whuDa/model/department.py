f1d18fb76edc4601184f09d8076e9b6f210e715b
80281 <2574897183@qq.com>
2016-07-13 10:34:29 +0800

    py by zyl

--- whuDa/model/department.py
+++ whuDa/model/department.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
 from whuDa import db
+'''department_id int(10) not null auto_increment comment '学院ID',
+    name varchar(32) not null comment '学院名','''
+class Department:
+	department_id=db.Column(db.Integer, primary_key=True);
+	name=db.Column(db.String(32), nullable=False);
--- whuDa/model/question_favorite.py
+++ whuDa/model/question_favorite.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 from whuDa import db
+'''question_favorite_id int(11) unsigned not null auto_increment comment '收藏的ID',
+    uid int(11) not null comment '收藏问题的用户UID',
+    question_id int(11) not null comment '收藏的问题ID',
+    add_time int(10) not null comment '收藏时间',
+    primary key (question_favorite_id)'''
+class Question_favorite:
+	question_favorite_id=db.Column(db.Integer, primary_key=True);
+	uid=db.Column(db.Integer,nullable=False);
+	question_id=db.Column(db.Integer,nullable=False);
+	add_time=db.Column(db.Integer,nullable=False);
