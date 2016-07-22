314540d28100447f516dd538023cb62860c89496
Terry <terrytu233@gmail.com>
2016-07-22 18:30:45 +0800

    后台中alert样式修改完成.

--- whuDa/static/admin/js/admin.js
+++ whuDa/static/admin/js/admin.js
@@ -57,36 +57,84 @@ function add_admin() {
     var website = $('#website').val()
 
     if (username == '') {
-        alert('用户名不能为空')
-        return
+        swal({
+            title: '添加失败',
+            text: '用户名不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (password == '') {
-        alert('密码不能为空')
-        return
+        swal({
+            title: '添加失败',
+            text: '密码不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (repeat_password == '') {
-        alert('请再次确认密码')
-        return
+        swal({
+            title: '添加失败',
+            text: '请再次输入密码',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (repeat_password != password) {
-        alert('两次输入的密码不一致，请检查')
-        return
+        swal({
+            title: '添加失败',
+            text: '两次输入的密码不一致，请检查',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (email == '') {
-        alert('邮箱不能为空')
-        return
+        swal({
+            title: '添加失败',
+            text: '邮箱不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (phone == '') {
-        alert('电话不能为空')
-        return
+        swal({
+            title: '添加失败',
+            text: '电话不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (!checkusername(username)) {
-        alert('用户名必须以字母开头')
-        return
+        swal({
+            title: '添加失败',
+            text: '用户名必须以字母开头',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (!checkmail(email)) {
-        alert('邮箱格式不正确，请重新输入')
-        return
+        swal({
+            title: '添加失败',
+            text: '邮箱格式不正确，请重新输入',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     document.forms["add_admin_form"].submit()
 }
@@ -107,36 +155,84 @@ function add_general_user() {
     var website = $('#website').val()
 
     if (username == '') {
-        alert('用户名不能为空')
-        return
+        swal({
+            title: '添加失败',
+            text: '用户名不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (password == '') {
-        alert('密码不能为空')
-        return
+        swal({
+            title: '添加失败',
+            text: '密码不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (repeat_password == '') {
-        alert('请再次确认密码')
-        return
+        swal({
+            title: '添加失败',
+            text: '请再次输入密码',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (repeat_password != password) {
-        alert('两次输入的密码不一致，请检查')
-        return
+        swal({
+            title: '添加失败',
+            text: '两次输入的密码不一致，请检查',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (email == '') {
-        alert('邮箱不能为空')
-        return
+        swal({
+            title: '添加失败',
+            text: '邮箱不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (phone == '') {
-        alert('电话不能为空')
-        return
+        swal({
+            title: '添加失败',
+            text: '电话不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (!checkusername(username)) {
-        alert('用户名必须以字母开头')
-        return
+        swal({
+            title: '添加失败',
+            text: '用户名必须以字母开头',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (!checkmail(email)) {
-        alert('邮箱格式不正确，请重新输入')
-        return
+        swal({
+            title: '添加失败',
+            text: '邮箱格式不正确，请重新输入',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     document.forms["add_user_form"].submit()
 }
@@ -157,36 +253,84 @@ function update_admin() {
     var website = $('#website').val()
 
     if (username == '') {
-        alert('用户名不能为空')
-        return
+        swal({
+            title: '修改失败',
+            text: '用户名不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (password == '') {
-        alert('密码不能为空')
-        return
+        swal({
+            title: '修改失败',
+            text: '密码不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (repeat_password == '') {
-        alert('请再次确认密码')
-        return
+        swal({
+            title: '修改失败',
+            text: '请再次输入密码',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (repeat_password != password) {
-        alert('两次输入的密码不一致，请检查')
-        return
+        swal({
+            title: '修改失败',
+            text: '两次输入的密码不一致，请检查',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (email == '') {
-        alert('邮箱不能为空')
-        return
+        swal({
+            title: '修改失败',
+            text: '邮箱不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (phone == '') {
-        alert('电话不能为空')
-        return
+        swal({
+            title: '修改失败',
+            text: '电话不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (!checkusername(username)) {
-        alert('用户名必须以字母开头')
-        return
+        swal({
+            title: '修改失败',
+            text: '用户名必须以字母开头',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (!checkmail(email)) {
-        alert('邮箱格式不正确，请重新输入')
-        return
+        swal({
+            title: '修改失败',
+            text: '邮箱格式不正确，请重新输入',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     document.forms["update_admin_form"].submit()
 }
@@ -207,58 +351,123 @@ function update_general_user() {
     var website = $('#website').val()
 
     if (username == '') {
-        alert('用户名不能为空')
-        return
+        swal({
+            title: '修改失败',
+            text: '用户名不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (password == '') {
-        alert('密码不能为空')
-        return
+        swal({
+            title: '修改失败',
+            text: '密码不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (repeat_password == '') {
-        alert('请再次确认密码')
-        return
+        swal({
+            title: '修改失败',
+            text: '请再次确认密码',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (repeat_password != password) {
-        alert('两次输入的密码不一致，请检查')
-        return
+        swal({
+            title: '修改失败',
+            text: '两次输入的密码不一致，请检查',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (email == '') {
-        alert('邮箱不能为空')
-        return
+        swal({
+            title: '修改失败',
+            text: '邮箱不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (phone == '') {
-        alert('电话不能为空')
-        return
+        swal({
+            title: '修改失败',
+            text: '电话不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (!checkusername(username)) {
-        alert('用户名必须以字母开头')
-        return
+        swal({
+            title: '修改失败',
+            text: '用户名必须以字母开头',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (!checkmail(email)) {
-        alert('邮箱格式不正确，请重新输入')
-        return
+        swal({
+            title: '修改失败',
+            text: '邮箱格式不正确',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     document.forms["update_user_form"].submit()
 }
 
 function deleteUser(obj) {
-    var r = confirm("确定要删除这个用户吗？请谨慎操作!");
-    if (r == true) {
+    swal({
+        title: '确定要删除吗？',
+        text: '该操作不可逆转，请谨慎使用',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "确定",
+        closeOnConfirm: false,
+        cancelButtonText: '取消'
+    }, function () {
         var uid = obj.getAttribute('data-uid')
         $.post('/admin/user/delete', {
             'uid':uid
         }, function (result) {
             if (result == 'success') {
-                alert('删除成功！');
-                location.reload();
-            } 
+                swal({
+                    title: '删除成功',
+                    type: 'success',
+                    confirmButtonText: '确定',
+                    confirmButtonColor: '#337ab7'
+                }, function () {
+                    location.reload();
+                });
+            }
             else {
-                alert('删除失败!');
+                swal({
+                    title: '删除失败',
+                    type: 'error',
+                    confirmButtonText: '确定',
+                    confirmButtonColor: '#337ab7'
+                });
             }
         })
-    } else {
-        location.reload()
-    }
+    })
 }
 
 function update_password() {
@@ -266,37 +475,75 @@ function update_password() {
     var new_pwd = $('#new_pwd').val()
     var repeat_new_pwd = $('#repeat_new_pwd').val()
     if (old_pwd == '') {
-        alert('原密码不能为空')
-        return
+        swal({
+            title: '修改失败',
+            text: '原密码不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (new_pwd == '') {
-        alert('新密码不能为空')
-        return
+        swal({
+            title: '修改失败',
+            text: '新密码不能为空',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (repeat_new_pwd == '') {
-        alert('请再次输入新密码')
-        return
+        swal({
+            title: '修改失败',
+            text: '请再次输入新密码',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     if (new_pwd != repeat_new_pwd) {
-        alert('两次输入的新密码不一致，请检查')
-        return
+        swal({
+            title: '修改失败',
+            text: '两次输入的新密码不一致，请检查',
+            type: 'error',
+            confirmButtonText: '确定',
+            confirmButtonColor: '#337ab7'
+        });
+        return false;
     }
     $.post('/admin/update_password', {
         'uid':uid,
         'old_pwd':old_pwd,
         'new_pwd':new_pwd,
         'repeat_new_pwd':repeat_new_pwd
-        }, function (result) {
-            if (result == 'success') {
-                alert('修改密码成功！');
+    }, function (result) {
+        if (result == 'success') {
+            swal({
+                title: '修改成功',
+                type: 'success',
+                confirmButtonText: '确定',
+                confirmButtonColor: '#337ab7'
+            }, function () {
                 if (flag == 1) {
                     location.href = '/admin/manage_admin/page/1'
                 } else {
                     location.href = '/admin/manage_user/page/1'
                 }
-            } 
-            else {
-                alert('原密码不正确，请重试');
-            }
-        })
+            });
+
+        }
+        else {
+            swal({
+                title: '修改失败',
+                text: '原密码不正确',
+                type: 'error',
+                confirmButtonText: '确定',
+                confirmButtonColor: '#337ab7'
+            });
+            return false;
+        }
+    })
 }
